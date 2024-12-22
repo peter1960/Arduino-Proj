1837ea65c6bd9a22cb8707d5ce79c810a55961be
peter1960 <pslowater@gmail.com>
2024-12-22 10:31:08 +0800

    Runs but needs hardware redesign, noise resonace from printed parts

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,25 @@
 #include "HX711.h"
 
 // HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 16; // GPIO16
-const int LOADCELL_SCK_PIN = 5;   // GPIO5
-const int LED_BLUE = 2;
+const int LOADCELL_DOUT_PIN = D0; // 16; // GPIO16
+const int LOADCELL_SCK_PIN = D1;  // 5;   // GPIO5
+const int LED_BLUE = D4;          // 2;
 // STEPPER
-const int STEP_PIN = 4;
-const int DIR_PIN = 0;
-const int SLEEP_PIN = 12;
+const int STEP_PIN = D2;
+const int DIR_PIN = D3;
+const int SLEEP_PIN = D6; // 12;
 
 HX711 scale;
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Bonezegei_A4988.h>
+#define FORWARD 1
+#define REVERSE 0
+
+Bonezegei_A4988 stepper(DIR_PIN, STEP_PIN);
+
 bool LED = false;
 
-void step()
+void xstep()
 {
   float st = 1;
   for (int x = 0; x < 800; x++)
@@ -25,11 +29,11 @@ void step()
     delay((int)st);
     digitalWrite(STEP_PIN, LOW);
     delay((int)st);
-    //st -= .01;
+    // st -= .01;
   }
 }
 
-void step_enable()
+void xstep_enable()
 {
   digitalWrite(SLEEP_PIN, LOW);
 }
@@ -42,11 +46,14 @@ void setup()
 {
   // put your setup code here, to run once:
   pinMode(LED_BLUE, OUTPUT);
-  pinMode(STEP_PIN, OUTPUT);
-  pinMode(DIR_PIN, OUTPUT);
-  pinMode(SLEEP_PIN, OUTPUT);
-    digitalWrite(DIR_PIN, HIGH);
-  step_disable();
+  // pinMode(STEP_PIN, OUTPUT);
+  // pinMode(DIR_PIN, OUTPUT);
+  // pinMode(SLEEP_PIN, OUTPUT);
+  //   digitalWrite(DIR_PIN, HIGH);
+
+  stepper.begin();
+  stepper.setSpeed(1000);
+  // step_disable();
   Serial.begin(57600);
   scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
   scale.set_scale(-188489 / 170);
@@ -79,10 +86,15 @@ void loop()
   {
     digitalWrite(LED_BLUE, HIGH);
     LED = true;
-    if (reading < 60){
-    step_enable();
-    step();
-    step_disable();
+    if (reading < 60)
+    {
+      stepper.step(FORWARD, 2000);
+      // stepper.disable();
+
+      // stepper.rotate(360);
+      //  step_enable();
+      //  step();
+      //  step_disable();
     }
   }
   delay(1500);
